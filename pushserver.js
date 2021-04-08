c11d542fcb6bc72afe47bda645b2b7623a868168
Martín San José de Vicente <martin@Martins-MacBook.home>
2021-04-08 10:10:11 +0200

    PushServer con ruta POST para enviar notificaciones push al cliente

--- pushserver.js
+++ pushserver.js
@@ -20,10 +20,69 @@ app.use(cors());
 
 // ************** RUTAS DE LA APLICACIÓN *********
 
-// - GET ('http://localhost:3000/') (PRUEBA de que funciona)
+// -> GET ('http://localhost:3000/') (PRUEBA de que funciona)
 app.get('/', (req, res) => {
-  res.send('Enviamos un mensaje')
+  // res.send('Probando API Restful'); // Mensaje plano
+  res.status(200).json({
+    mensaje: 'Probando API Restful'
+  });
 });
+
+// -> POST ('http://localhost:3000/subscribe')
+// Ruta para que el cliente se suscriba a las notificaciones push
+// a través de Web Push
+app.post('/subscribe', (req, res) => {
+
+  // Obtenemos el BODY de la petición POST
+  let body = req.body;
+
+  // Definimos las claves Públicas y Privadas de Web-Push
+  // Las vamos a obtener a través del comando:
+  // ** web-push generate-vapid-keys
+  // * Pública:
+  // BFdgtv35vCLg4Hqd-I8dzsR2wZzdWj_zqycW9bkXUp_TPGu-aYflPdx8fmPyt2837BoeYFU_sC8Khdp_kvmzQSU
+  // * Privada:
+  // iZsmV0KJJ0ltYGeb90x4CliHFKwAvf7JJ9pTep7vJeg
+
+  // Configuramos WebPush con las claves y el email al que mandar un mensaje
+  webpush.setVapidDetails(
+    'mailto:martin@imaginagroup.com',
+    'BFdgtv35vCLg4Hqd-I8dzsR2wZzdWj_zqycW9bkXUp_TPGu-aYflPdx8fmPyt2837BoeYFU_sC8Khdp_kvmzQSU',
+    'iZsmV0KJJ0ltYGeb90x4CliHFKwAvf7JJ9pTep7vJeg'
+  );
+
+
+  // Configuramos La Notificación Push que vamos a enviar al cliente (Angular)
+  // Este es el contenido que le llegará en el mensaje (título, cuerpo del mensaje e icono)
+  let payload = JSON.stringify({
+    "notification": {
+      "title": "Martín API Restful",
+      "body": "Gracias por suscribirte a nuestra NewsLetter",
+      "icon": "https://image.pngaaa.com/128/3000128-middle.png"
+    }
+  });
+
+  // Enviar la notificación al cliente y cuando se haya enviado, entonces
+  // enviamos un mensaje de éxito
+  Promise.resolve(
+    webpush.sendNotification(body, payload)
+  ).then(() => {
+    // Si todo ha ido bien:
+    res.status(200).json({
+      mensaje: 'Notificación enviada con éxito'
+    });
+  }).catch(err => {
+    // Si algo ha salido mal:
+    console.log('Error en el servidor:', err);
+    res.status(500).json({
+      mensaje: 'Error en el servidor',
+      error: err
+    })
+  });
+})
+
+
+
 
 
 
